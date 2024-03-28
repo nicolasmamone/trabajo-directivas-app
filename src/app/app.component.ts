a0ab5be177abf9342eb5a99735f95242c6abd0b1
Nico Mamone <nicolasmamone@gmail.com>
2024-03-28 17:14:09 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajo-directivas-app';
+  titulo = 'Registro De Usuarios';
+  mensaje = '';
+  registrado = false;
+  nombre: string = '';
+  apellido: string = '';
+  cargo: string = '';
+  //entradas: object[];
+  entradas: { nombre: string, apellido: string }[] = [];
+  constructor(){
+    this.entradas = [
+      {nombre: 'Juan', apellido: 'Perez'},
+      {nombre: 'Maria', apellido: 'Gomez'},
+      {nombre: 'Pedro', apellido: 'Rodriguez'},
+      {nombre: 'Luis', apellido: 'Gonzalez'},
+      {nombre: 'Ana', apellido: 'Lopez'},
+      {nombre: 'Jose', apellido: 'Martinez'}
+    ];
+  
+  }
+
+  registrarUsuario(){
+    this.registrado = !this.registrado;
+    this.mensaje = 'Usuario registrado con exito';
+  }
 }
